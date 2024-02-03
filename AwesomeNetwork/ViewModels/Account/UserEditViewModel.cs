cd61faa66e360e956f8732d187aad8d381b1e82b
valmon55 <fedor_ka@mail.ru>
2024-02-03 21:37:22 +0300

    Обновление работает

--- AwesomeNetwork/ViewModels/Account/UserEditViewModel.cs
+++ AwesomeNetwork/ViewModels/Account/UserEditViewModel.cs
@@ -10,20 +10,38 @@ namespace AwesomeNetwork.ViewModels.Account
     public class UserEditViewModel
     {
         [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "ID", Prompt = "ID пользователя")]
         public string UserId { get; set; }
         [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Фото", Prompt = "URL фотографии")]
         public string Image { get; set; }
         [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Фамилия", Prompt = "Укажите свою фамилию")]
         public string LastName { get; set; }
         [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Отчество", Prompt = "Укажите своё отчество")]
         public string MiddleName { get; set; }
         [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Имя", Prompt = "Укажите своё имя")]
         public string FirstName { get; set; }
         [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Email", Prompt = "Укажите свой Email")]
         public string Email { get; set; }
         [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Дата рождения", Prompt = "Укажите вашу дату рождения")]
         public DateTime BirthDate { get; set; }
+        [DataType(DataType.Text)]
+        [Display(Name = "Nick Name", Prompt = "Укажите имя пользователя")]
         public string UserName => Email;
+        [DataType(DataType.Text)]
+        [Display(Name = "Статус", Prompt = "Введите свой статус")]
         public string Status { get; set; }
         [DataType(DataType.Text)]
         [Display(Name = "О себе", Prompt = "Введите данные о себе")]
